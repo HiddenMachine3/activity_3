86075a69d7d7fe54a567b717b1eaa65179be3b24
HiddenMachine3 <suhashmsocial@gmail.com>
2025-09-29 23:18:34 +0530

    removed red background

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ A simple streamlit calculator app.
 import streamlit as st
 from funcs import add, sub, mult
 
-st.markdown("""<style>.stApp { background-color: red; }</style>""", unsafe_allow_html=True)
 
 a = st.number_input("a:", value=0)
 b = st.number_input("b:", value=0)
