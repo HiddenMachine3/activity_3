91071892f7eb24cc7339cd2c16e0dd2aa810737e
HiddenMachine3 <suhashmsocial@gmail.com>
2025-09-30 08:30:15 +0530

    deleted file, added title

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import streamlit as st
 from funcs import add, sub, mult
 
 
+st.title("Calculator App")
 a = st.number_input("a:", value=0)
 b = st.number_input("b:", value=0)
 operator = st.selectbox("operator:", ["+", "-", "*"])
